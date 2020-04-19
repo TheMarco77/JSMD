dc7143ff95cc2c5f2bbbb605c9828c47b178df2a
Marco F. Ramos <mfilipe@gmail.com>
2020-04-19 13:37:27 -0400

    JSMD HTML(2) document.write

--- JS/conditionals.js
+++ JS/conditionals.js
@@ -4,6 +4,6 @@ if ( answer.toUpperCase() === 'MERCURY' ) {
   document.write('<p><h1 style="text-align: center;">That\'s correct!</h1></p>');
   console.log("That's correct!");
 } else {
-  document.write('<h1>Sorry, that\'s incorrect!</h1>');
+  document.write('<p><h1 style="text-align: center;">Sorry, that\'s incorrect!</h1></p>');
   console.log("Sorry, that's incorrect!");
 }
