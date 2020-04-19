4d846bd21d9a5a6366ef2368c6a21320dde03339
Marco F. Ramos <mfilipe@gmail.com>
2020-04-19 13:14:37 -0400

    JSMD HTML document.write

--- JS/conditionals.js
+++ JS/conditionals.js
@@ -1,7 +1,9 @@
 const answer = prompt('Which planet is closest to the sun?');
 
 if ( answer.toUpperCase() === 'MERCURY' ) {
-  console.log("That's correct!");  
+  document.write('<p><h1 style="text-align: center;">That\'s correct!</h1></p>');
+  console.log("That's correct!");
 } else {
-  console.log("Sorry, that's incorrect");
+  document.write('<h1>Sorry, that\'s incorrect!</h1>');
+  console.log("Sorry, that's incorrect!");
 }
